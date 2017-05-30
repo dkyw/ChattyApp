9a45ccd1a9be87dce09360b0a878f55487c1b6a5
dkyw <derrellwong@gmail.com>
2017-05-30 23:26:16 +0000

    pass messages as prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
 
 
-// import Message from './Message.jsx'
 import MessageList from './MessageList.jsx'
 import ChatBar from './ChatBar.jsx'
 
 class App extends Component {
+  constructor(props) {
+  super(props);
+    this.state = {
+      currentUser: {name: 'Bob'},
+      messages: [
+        {
+        username: "Bob",
+        content: "Has anyone seen my marbles?",
+      },
+      {
+        username: "Anonymous",
+        content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+      }
+    ]
+      }
+  }
+
   render() {
     return (
       <div>
-        <MessageList />
-        <ChatBar />
+        <MessageList messages = {this.state.messages} />
+        <ChatBar currentUser = {this.state.currentUser} />
       </div>
     );
   }
 }
 
 export default App;
+
+
