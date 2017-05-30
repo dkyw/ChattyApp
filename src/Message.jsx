d8a795394f80cb1d43dc8f476be5320b7149dafb
dkyw <derrellwong@gmail.com>
2017-05-30 23:27:11 +0000

    pass message as props

--- src/Message.jsx
+++ src/Message.jsx
@@ -5,11 +5,13 @@ class Message extends Component {
     return (
       <main className='messages'>
         <div className="message">
-          <span className="message-username">Anonymous1</span>
-          <span className="message-content">I won't be impressed with technology until I can download food.</span>
+          <span className="message-username">{this.props.key}{this.props.username}</span>
+          <span className="message-content">{this.props.content}</span>
         </div>
       </main>
     );
   }
 }
 export default Message;
+
+
