eaf2a28fc4779a876b74852f1d42cd53ea66eb0e
dkyw <derrellwong@gmail.com>
2017-05-30 23:36:58 +0000

    added componentDidMount

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import ChatBar from './ChatBar.jsx'
 
 class App extends Component {
   constructor(props) {
-  super(props);
+    super(props);
     this.state = {
       currentUser: {name: 'Bob'},
       messages: [
-        {
+      {
         username: "Bob",
         content: "Has anyone seen my marbles?",
       },
@@ -18,9 +18,27 @@ class App extends Component {
         username: "Anonymous",
         content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
       }
-    ]
-      }
+      ]
+    }
   }
+
+  // in App.jsx
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    const messages = this.state.messages.concat(newMessage)
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.
+    this.setState({messages: messages})
+      }, 3000);
+    }
+    render() {
+  // more code here..
+  }
+
 
   render() {
     return (
@@ -28,9 +46,13 @@ class App extends Component {
         <MessageList messages = {this.state.messages} />
         <ChatBar currentUser = {this.state.currentUser} />
       </div>
-    );
+      );
+    }
   }
-}
+
+
+
+
 
 export default App;
 
