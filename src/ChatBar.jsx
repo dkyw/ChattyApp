6372a42ea32fa5251055560b1225e26f83aca713
dkyw <derrellwong@gmail.com>
2017-05-31 05:17:56 +0000

    functions for handling user input

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,21 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 
 class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ''
+      }
+    }
 
+  onKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      this.props.onNewMessage(this.state.content);
+      this.setState ({ content: '' })
+    }
+  }
+
+  onMessageChange = (event) => {
+    this.setState({ content: event.target.value });
+  }
+
+  onNameChange = (event) => {
+    this.props.onNameChange(event.target.value);
+  }
 
 
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-    </footer>
-    );
+        <input
+          className="chatbar-username"
+          placeholder="Your Name (Optional)"
+          defaultValue={ this.props.currentUser ? this.props.currentUser.name : undefined}
+          onChange={ this.onNameChange }
+           />
+        <input
+          className="chatbar-message"
+          placeholder ="Type a message and hit ENTER"
+          onKeyDown= { this.onKeyPress }
+          onChange = { this.onMessageChange }
+          value ={ this.state.content }
+          />
+      </footer>
+    )
   }
 }
+
 export default ChatBar;
-
-
-// this.props.currentUser
-
-
