30ee4cd3e7172b2358b9092fc8d0dcbbb5b18496
dkyw <derrellwong@gmail.com>
2017-05-31 16:33:20 -0700

    installed UID pkg

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,18 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
+
+const uniqueID = require('uuid/v4');
+
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       currentUser: { name: "Bob" },
-      messages: [
-        {
-          id: 1,
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          id: 2,
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      messages: []
     }
+
     this.onNewMessage = this.onNewMessage.bind(this)
     this.socket = new WebSocket('ws://localhost:3001')
   }
@@ -29,7 +22,7 @@ class App extends Component {
 
   onNewMessage(content, username) {
     const newMessage = {
-      id: Math.random(),
+      id: uniqueID(),
       username: username || this.state.currentUser.name,
       content: content
     }
