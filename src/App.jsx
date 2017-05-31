7f57261c516bba259a200ceaf1d81e42d9d08396
dkyw <derrellwong@gmail.com>
2017-05-31 05:17:40 +0000

    functions for recieving message content and username

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,58 @@
 import React, {Component} from 'react';
-
-
-import MessageList from './MessageList.jsx'
-import ChatBar from './ChatBar.jsx'
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: {name: 'Bob'},
+      currentUser: { name: "Bob" },
       messages: [
-      {
-        username: "Bob",
-        content: "Has anyone seen my marbles?",
-      },
-      {
-        username: "Anonymous",
-        content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-      }
+        {
+          id: 1,
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: 2,
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
       ]
     }
+    this.onNewMessage = this.onNewMessage.bind(this)
+    this.onNameChange = this.onNameChange.bind(this)
   }
 
-  // in App.jsx
-  componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-      }, 3000);
-    }
-    render() {
-  // more code here..
+
+  onNewMessage(content) {
+    const newMessage = {id: this.state.messages.id, username: this.state.currentUser.name, content: content }
+    this.setState({
+      messages: this.state.messages.concat(newMessage)
+    })
   }
 
+  onNameChange(username) {
+    this.setState({currentUser: {name: username}})
+  }
 
   render() {
+    const { currentUser, messages } = this.state;
     return (
       <div>
-        <MessageList messages = {this.state.messages} />
-        <ChatBar currentUser = {this.state.currentUser} />
+        <nav className="navbar-brand"> </nav>
+        <MessageList messages = { messages } />
+        <ChatBar
+          // currentUser =  { this.state.currentUser }
+          currentUser = { currentUser }
+          onNewMessage = { this.onNewMessage }
+          onNameChange = { this.onNameChange }
+        />
       </div>
-      );
-    }
+    );
   }
-
-
-
+}
 
 
 export default App;
-
-
