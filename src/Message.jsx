bdc6a639e9d7267b78f5276a5279c537242df732
dkyw <derrellwong@gmail.com>
2017-06-01 23:26:19 +0000

    added css style for changing name

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,16 +2,26 @@ import React, {Component} from 'react';
 
 class Message extends Component {
   render() {
-    return (
-      <main className='messages'>
-        <div className="message">
-          <span className="message-username"> { this.props.username }</span>
-          <span className="message-content">{this.props.content}</span>
-        </div>
-      </main>
-    );
+    if (this.props.type === "postMessage") {
+      return (
+        <main className='messages'>
+          <div className="message">
+            <span className="message-username"> { this.props.username }</span>
+            <span className="message-content">{this.props.content}</span>
+          </div>
+        </main>
+      );
+
+    } else if (this.props.type === 'postNotification') {
+      return (
+        <main className='messages'>
+          <div class="message system">{this.props.content}</div>
+        </main>
+      );
+    }
   }
 }
+
 export default Message;
 
 
